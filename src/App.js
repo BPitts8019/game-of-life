99c32d092228e2b6dc014e346badf0f3b1beaa50
BPitts8019 <bradleyj.pitts@gmail.com>
2020-07-22 18:48:02 -0700

    feat: Create a Cell that a user can interact with

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Cell from "./features/cell/Cell";
 
 function App() {
    return (
       <div className="App">
          <h1>The Game of Life</h1>
          <h3>by John Conway</h3>
+         <div className="game-board">
+            <Cell />
+            <Cell />
+         </div>
       </div>
    );
 }
