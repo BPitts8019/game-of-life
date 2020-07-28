8c6728f0759212bbcb36b56f702800cce652e514
BPitts8019 <bradleyj.pitts@gmail.com>
2020-07-27 19:16:17 -0700

    chore: Remove React-Redux

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./app/store";
 import GlobalStyle from "./features/style-utils/GlobalStyle";
-import { Provider } from "react-redux";
 
 ReactDOM.render(
    <React.StrictMode>
       <GlobalStyle />
-      <Provider store={store}>
-         <App />
-      </Provider>
+      <App />
    </React.StrictMode>,
    document.getElementById("root")
 );
