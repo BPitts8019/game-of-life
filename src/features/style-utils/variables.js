a765ad66aaec545eee266b00044391be60f8ba9c
BPitts8019 <bradleyj.pitts@gmail.com>
2020-07-30 19:13:36 -0700

    chore: Cleanup style utilities

--- src/features/style-utils/variables.js
+++ src/features/style-utils/variables.js
@@ -1,57 +1,14 @@
 //*** Constants ***/
 export const MAX_WIDTH = "1040px";
-export const MIN_WIDTH = "400px";
+export const MIN_WIDTH = "660px";
 
 //*** Fonts ***//
 export const headerFont = `'Oswald', Sans-Serif`;
 export const bodyFont = `'Quattrocento', Serif`;
 
-//*** Colors ***//
-const palette = [
-   "#000000", // black [0]
-   "#ffffff", // white [1]
-   "#eeeeee", // color [2]
-   "#dddddd", // color [3]
-   "#bbbbbb", // color [4]
-   "#999999", // color [5]
-];
-const alpha = "ff";
-
 //*** Named Colors ***//
-const darkText = palette[0];
-const lightText = palette[1];
-const attentionColor = palette[1];
-const attentionBorder = palette[1];
-const subtleBorder = palette[0];
-const bgColor = palette[5];
-const darkBackground = palette[5];
-const accent = palette[3];
-
-// in global styles
-const headerColor = attentionColor;
-const bodyColor = bgColor;
-// in global styles can change properties but not the name.
-// const overlayBackground = [
-//   accent + alpha,
-//   attentionBorder + alpha,
-//   attentionColor + alpha
-// ];
-const link = darkText;
-const linkVisited = darkText;
-const linkHover = subtleBorder;
+const bodyColor = "#999999";
 
 export const colors = {
-   attentionColor,
-   attentionBorder,
-   subtleBorder,
-   bgColor,
-   accent,
-   darkText,
-   lightText,
-   darkBckgrnd: darkBackground,
-   headerColor,
    bodyColor,
-   link,
-   linkVisited,
-   linkHover,
 };
